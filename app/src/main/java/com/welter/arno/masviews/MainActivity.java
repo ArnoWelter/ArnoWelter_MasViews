7696d44eb38bb1cce03f8d98a78ded84636f0c6f
arnow_000 <arnowelter03@gmail.com>
2016-10-15 17:39:52 +0200

    Ejercicio 2.10

--- app/src/main/java/com/welter/arno/masviews/MainActivity.java
+++ app/src/main/java/com/welter/arno/masviews/MainActivity.java
@@ -2,19 +2,39 @@ package com.welter.arno.masviews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText entrada;
+    private TextView salida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        entrada = (EditText)findViewById(R.id.entrada);
+        salida = (TextView)findViewById(R.id.salida);
+
     }
 
     public void sePulsa(View view){
-        Toast.makeText(this, "Pulsado", Toast.LENGTH_SHORT).show();
+        String entrada2 = entrada.getText().toString();
+        if (entrada2.equals("")){
+            Toast.makeText(getApplicationContext(),"Introduzca un numero antes de pulsar el boton rojo", Toast.LENGTH_SHORT).show();
+        }else{
+            salida.setText(String.valueOf(Float.parseFloat(entrada.getText().toString()) * 2.0));
+        }
     }
+
+    public void sePulsa0(View view){
+        entrada.setText(entrada.getText() + (String)view.getTag());
+    }
+
 
 }
